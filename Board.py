87a1eb372b840a2850dee59787e7ca3595d7ef1c
Michael O'Connor <michaeloconnor1717@gmail.com>
2022-09-27 11:12:52 -0400

    setup small board array system. began naive approach to board checking

--- Board.py
+++ Board.py
@@ -2,19 +2,46 @@ import copy
 
 
 class Board:
+    #state of big board
     boardArray = [-1]*81
-    lastMove = (-1, -1)
+    closedBoards = [(False, 0)]*9 #closed state and winner (0 for X, 1 for O)
+    moves = []
     currMove = 0  #Current move in the game (so the move the agent will returm) i.e. if the last move was 4, current move will be five
+   
+
+   #state of small boards
+    smallBoardPnts = [[[0,0,0], [0,0,0], [0,0]] for i in range(9)]
+
+   
     def Board(initMoves: list, self):
         for i in range(0, len(initMoves)):
             self.boardArray[initMoves[i][0]*9+initMoves[i][1]] = self.currMove%2
+
             self.currMove +=1
-        self.lastMove = initMoves[-1]
+        self.moves.append(copy.deepcopy(initMoves[-1]))
+        
 
     def newMove(move:list, self):
         self.boardArray[move[0] * 9 + move[1]] = self.currMove%2
-        self.lastMove = copy.deepcopy(move)
+        self.moves.append(copy.deepcopy(move))
         self.currMove +=1
 
     def legalMoves(self):
-        return i for i, n in enumerate()
+        legalMoves = ()
+        if(self.closedBoards[self.lastMove[1]][0]):
+            for i in range(0, 81):
+                if self.BoardArray[i]==-1:
+                    legalMoves.append(self.boardArray[i])
+        else:
+            for i in range(0,9):
+                if(self.boardArray[self.lastMove[1]*9+i]==-1):
+                    legalMoves.append(self.lastMove[1]*9+i)
+        return legalMoves
+    
+    def rollBackMove(self):
+        self.currMove -= 1
+        self.moves.pop()
+    
+    # def checkSmallBoards(self):
+    #     for i in range(0, 9):
+
