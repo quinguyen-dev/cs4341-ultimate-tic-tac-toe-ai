edf2f6458baa42b8aa7ba29f5e1dbf926c1e884a
Michael O'Connor <michaeloconnor1717@gmail.com>
2022-10-01 00:53:23 -0400

    added threading for searching

--- Proj-2-ref/aa-app/aa-main.py
+++ Proj-2-ref/aa-app/aa-main.py
@@ -7,19 +7,21 @@ from utility import State
 
 def main():
     player = Player("artificial_andys")
-    board = Board() 
+    board = Board()
+    ai = AI()
 
     print(f'[Initialized player]: {player.team_name}')           
 
     while not player.check_end():  
         player.read_first_four(board)  
               
-        if player.check_for_turn():  
+        if player.check_for_turn():
+            print('is turn')  
             player.read_move(board)
 
             opponent_move = player.last_move
             print(player.last_move)
-            player_move = AI.determine_move(board, opponent_move) 
+            player_move = ai.thread_determine_move(board, opponent_move) 
 
             board.new_move(player_move)
             player.make_move(player_move)
