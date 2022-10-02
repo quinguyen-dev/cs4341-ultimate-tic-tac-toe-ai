5d4252967f416226a327a366fed4a985f3b527db
Michael O'Connor <michaeloconnor1717@gmail.com>
2022-10-01 20:50:37 -0400

    init implement random agent

---
+++ Proj-2-ref/aa-app/random_bot.py
@@ -0,0 +1,32 @@
+from random import random
+from player import Player
+from Board import Board
+from time import sleep
+from threading import Thread
+from ai import AI
+from utility import State
+
+def main():
+    player = Player("bot")
+    board = Board() 
+
+    print(f'[Initialized player]: {player.team_name}')           
+
+    while not player.check_end():  
+        player.read_first_four(board)  
+              
+        if player.check_for_turn():  
+            player.read_move(board)
+
+            opponent_move = player.last_move
+            print(player.last_move)
+            
+            player_move = random.choice(board.legal_moves(opponent_move))
+            
+            board.new_move(player_move)
+            player.make_move(player_move)
+            
+            print("\n=============================")
+
+if __name__ == '__main__':
+    main()
